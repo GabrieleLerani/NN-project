875c9246d965c9181aa445097642f3275d2dafb3
GabrieleLerani <gabriele.lerani2000@gmail.com>
2024-08-23 13:30:05 +0200

    Generalization to N dimension

--- ckconv/nn/ck/magnet.py
+++ ckconv/nn/ck/magnet.py
@@ -104,113 +104,82 @@ class AnisotropicGaborLayer(nn.Module):
             bias=True,
         )
 
-        self.gamma_x = nn.Parameter(
-            torch.distributions.gamma.Gamma(alpha / (current_layer + 1), beta).sample(
+    
+        gamma_dist = torch.distributions.gamma.Gamma(alpha / (current_layer + 1), beta).sample(
                 (hidden_channels, 1)
             )
-        )
 
-        self.gamma_y = nn.Parameter(
-            torch.distributions.gamma.Gamma(alpha / (current_layer + 1), beta).sample(
+        self.gamma = nn.ParameterList([
+            nn.Parameter(gamma_dist.sample(
                 (hidden_channels, 1)
-            )
-        )
+            )) for _ in range(data_dim)
+        ])
 
-        # they are initialized according to normal distribution
-        normal_dist = torch.distributions.normal.Normal(
+        normal_dist = torch.distributions.normal.Normal( # TODO
             0, 1
-        )  # Create a normal distribution with mean 0 and std 1
-        self.mi_x = nn.Parameter(normal_dist.sample((hidden_channels, 1)))
-        self.mi_y = nn.Parameter(normal_dist.sample((hidden_channels, 1)))
+        )
 
+        self.mi = nn.ParameterList([
+            nn.Parameter(normal_dist.sample((hidden_channels,1))) for _ in range(data_dim)
+        ])
+        
+        # TODO init weights and biases    
         # init the frequency components W_g for the orientation and frequency of sinusoidal and b for phase offset
 
         # self.linear.weight.data = nn.Parameter(torch.randn(hidden_channels,data_dim,1,1))
         # self.linear.bias = nn.Parameter(torch.randn(hidden_channels))
 
-        self.linear.weight.data *= self.gamma_x.view(
-            *self.gamma_x.shape, *((1,) * data_dim)
-        )
+        # self.linear.weight.data *= self.gamma_x.view(
+        #     *self.gamma_x.shape, *((1,) * data_dim)
+        # )
 
-        print(f"Size {self.linear.weight.shape}")
+        # print(f"Size {self.linear.weight.shape}")
 
-        self.linear.bias.data.fill_(0.0)
+        # self.linear.bias.data.fill_(0.0)
 
     def forward(self, x):
         """
-        Standard method of nn.modules
-        We implement the 2D gabor function in the paper
-        We assume the input is x of shape [batch_size,2]
+        
         """
-        # [1,2,20,20]
-        # coord = []
-        # for i in range(self.data_dim):
-        #     coord.append(self.x[0][i])
-
-        x_coord = x[0, 0, :, :]
-        y_coord = x[0, 1, :, :]
+        
+    
+        # coordinates (x,y,...)
+        coord = [ x[0][i] for i in range(self.data_dim)]
 
         # reshaping the parameters
-        reshaped_x = x_coord.view(1, 1, *x_coord.shape)
-        reshaped_y = y_coord.view(1, 1, *y_coord.shape)
+        reshaped_coord = [ c.view(1,1,1,*c.shape) for c in coord]
 
-        reshaped_gamma_x = self.gamma_x.view(
-            1, *self.gamma_x.shape, *((1,) * (self.data_dim - 1))
-        )
+        reshaped_gamma = [g.view(1, *g.shape, *((1,) * (self.data_dim)))  for g in self.gamma]
+        reshaped_mi = [m.view(1, *m.shape, *((1,) * (self.data_dim)))  for m in self.mi]
 
-        reshaped_gamma_y = self.gamma_y.view(
-            1, *self.gamma_y.shape, *((1,) * (self.data_dim - 1))
-        )
 
-        reshaped_mi_x = self.mi_x.view(
-            1, *self.mi_x.shape, *((1,) * (self.data_dim - 1))
-        )
+        g_envelopes = []
+        for i in range(data_dim):
+            g_envelope = torch.exp(
+                -0.5 * (reshaped_gamma[i] * (reshaped_coord[i] - reshaped_mi[i])) ** 2
+            )  # Shape: [1, hidden_channels, 20, 20]
+            g_envelopes.append(g_envelope)
+        
+        # Multiply all the envelopes together
+        g_envelope = g_envelopes[0]
+        for i in range(1, data_dim):
+            g_envelope *= g_envelopes[i]
+        
+        # Squeeze the third dimension
+        g_envelope = g_envelope.squeeze(2)
 
-        reshaped_mi_y = self.mi_y.view(
-            1, *self.mi_y.shape, *((1,) * (self.data_dim - 1))
-        )
-        print(f"mix {reshaped_mi_x.shape}")
-        g_envelope_x = torch.exp(
-            -0.5 * (reshaped_gamma_x * (reshaped_x - reshaped_mi_x)) ** 2
-        )  # Shape: [1, hidden_channels, 20, 20]
-
-        g_envelope_y = torch.exp(
-            -0.5 * (reshaped_gamma_y * (reshaped_y - reshaped_mi_y)) ** 2
-        )  # Shape: [1, hidden_channels, 20, 20]
-
-        g_envelope = g_envelope_x * g_envelope_y
-
-        # TODO error gaussian envelope shape must be [1, hidden_channels, 20, 20] and not [1, hidden_channels,2, 20, 20])
-
-        # reshaped_gamma_x = self.gamma_x.view(1, *self.gamma_x.shape, *((1,) * self.data_dim)) # -> 1, a, b, 1, 1, 1)
-        # reshaped_gamma_y = self.gamma_y.view(1, *self.gamma_y.shape, *((1,) * self.data_dim))
-
-        # reshaped_mi_x = self.mi_x.view(1, *self.mi_x.shape, *((1,) * self.data_dim))
-        # reshaped_mi_y = self.mi_y.view(1, *self.mi_y.shape, *((1,) * self.data_dim))
-
-        # computing the gaussian envelope
-        # g_envelope = torch.exp(
-        #     -0.5
-        #     * (
-        #         (reshaped_gamma_x * (x.unsqueeze(1) - reshaped_mi_x)) ** 2
-        #         + (reshaped_gamma_y * (x.unsqueeze(1) - reshaped_mi_y)) ** 2
-        #     )
-        # )
 
         # computing the sinusoidal
         sinusoidal = torch.sin(self.linear(x))
 
-        print(f"g_envelope: {g_envelope.shape}")
-        print(f"sinusoidal shape: {x.shape}")
-
         return g_envelope * sinusoidal
 
-
+# TODO move in a separate file
 def linspace_grid(grid_sizes):
     """Generates a flattened grid of (x,y,...) coordinates in a range of -1 to 1."""
     tensors = []
     for size in grid_sizes:
-        tensors.append(torch.linspace(-1, 1, steps=size))
+        tensors.append(torch.arange(size).float())
 
     grid = torch.stack(torch.meshgrid(*tensors), dim=0)
 
@@ -220,15 +189,16 @@ def linspace_grid(grid_sizes):
 if __name__ == "__main__":
     # test the model
     data_dim = 2
-    model = MAGNet(data_dim=data_dim, hidden_channels=32, out_channels=50, no_layers=3)
+    model = MAGNet(data_dim=data_dim, hidden_channels=2, out_channels=2, no_layers=3)
 
     x = linspace_grid(
         [
-            20,
+            2,
         ]
         * data_dim
     ).unsqueeze(0)
     print(f"grid shape: {x.shape}")
-
+    print(x)
     x = model(x)
+    print(f"results : {x}")
     print(x.shape)
